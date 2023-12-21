0a0e02c1c3026ad9f9c3935fa5c9a46798a7f288
Aaron Suggs <ktheory@users.noreply.github.com>
2023-12-21 10:53:09 -0500

    day 11

--- 2023/11.rb
+++ 2023/11.rb
@@ -1,7 +1,37 @@
-data = STDIN.readlines.map(&:chars)
+data = STDIN.readlines.map{|l| l.strip.chars }
 
 require 'set'
 
-expanded_rows = (0...data.size).select{|row| row.uniq == ['.']}.to_set
-expanded_cols = (0...data.first.size).map{|row| row[i]}.select{|col| col.uniq == ['.']}.to_set
+$expanded_rows = (0...data.size).select{|i| data[i].uniq == ['.']}.to_set
+$expanded_cols = (0...data.first.size).select{|j| data.map{|r| r[j]}.uniq == ['.']}.to_set
 
+galaxies = data.each_with_index.reduce([]) { |acc, (row, i)|
+  row.each_with_index.reduce(acc) { |acc, (col, j)|
+    acc << [i, j] if col == '#'
+    acc
+  }
+}
+
+# puts expanded_rows.inspect
+# puts expanded_cols.inspect
+# puts galaxies.inspect
+def distance((x1,y1), (x2,y2), empty_factor = 1)
+  x = Range.new(*[x1,x2].sort, true)
+  y = Range.new(*[y1,y2].sort, true)
+
+  x.select{|i| $expanded_rows.include?(i)}.size*empty_factor + \
+  y.select{|j| $expanded_cols.include?(j)}.size*empty_factor + \
+  x.size + y.size
+end
+
+
+puts galaxies.combination(2).map{|a,b |
+  # puts "#{a} #{b} #{distance(a,b)}"
+  distance(a,b)
+}.sum
+
+# Part 2:
+puts galaxies.combination(2).map{|a,b |
+  # 649863639480 -- too high
+  distance(a,b, 1_000_000-1)
+}.sum
